b0bca58bb68b19e445f4f92bf277bd2c57b2970d
Cameron Mckeown <pdxcameron@gmail.com>
2020-08-18 14:07:08 -0700

    Joaquinmiller.com

---
+++ backupfiles/new/discussions/config.php
@@ -0,0 +1,18 @@
+<?php
+
+
+// phpBB 2.x auto-generated config file
+// Do not change anything in this file!
+
+$dbms = 'mysql4';
+
+$dbhost = 'db965.perfora.net';
+$dbname = 'db206311433';
+$dbuser = 'dbo206311433';
+$dbpasswd = 'ZHJWv4hU';
+
+$table_prefix = 'phpbb_';
+
+define('PHPBB_INSTALLED', true);
+
+?>
